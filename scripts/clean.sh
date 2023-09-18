a3a7d360b76cd1842a9217fc8c5c4023cebaf8b7
VinceHPicton <vince.picton@gmail.com>
2023-09-18 12:27:58 +0100

    wip

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -2,8 +2,11 @@
 
 . ./config/env.sh
 
-docker-compose -f ./docker-compose.yml down
+docker-compose -f ./docker-compose.yml down --volumes
+docker volume prune -f
 docker rmi -f ${PSQL_CLI_IMAGE_NAME}
 docker rmi -f $(docker images --filter "dangling=true" -q)
 docker rmi -f ${GOAPP_IMAGE_NAME}
-rm -rf ./tmp
+
+# chmod -R +w ./tmp
+# rm -rf ./tmp
